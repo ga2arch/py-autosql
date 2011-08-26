de22aee887af4d035994c7a9eea21e11ff7b8b0a
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2011-08-26 17:04:58 +0200

    bugfixes

--- sql.py
+++ sql.py
@@ -32,9 +32,9 @@ class SQLDB:
     def __get_table_schema(self, table):
         sql = 'select sql from sqlite_master where type = \'table\' and name = ?'
         self.c.execute(sql, (table, ))
-        result = self.c.fetchone()[0]
-        if not result: raise Exception('Wrong table name')
-        return result
+        result = self.c.fetchone()
+        if not result: raise Exception('Table doesn\'t exists')
+        return result[0]
     
     def __add_column(self, table, field, field_type):
         sql = 'alter table %s add %s %s;' % (table, field, field_type)
@@ -65,8 +65,8 @@ class SQLDB:
             try:self.__add_column(table, field, field_type)
             except:pass
     
-    def insert_or_update(self, table, data, check_field):
-        self.__create_table_if_not_exists(table)
+    def save(self, table, data, check_field, table_should_exists=False):
+        if not table_should_exists: self.__create_table_if_not_exists(table)
         keys, values = self.__reorder_values(table, data)
         self.__alter_table(table, keys, data)
         keys, values = self.__reorder_values(table, data)
@@ -89,4 +89,4 @@ class SQLDB:
         return self.c.fetchone()[0]
 
 db = SQLDB('test.sqlite')
-db.insert_or_update('Low', dict(nome='Luca'), 'nome')   
+db.save('Lw', dict(nome='Luca'), 'nome', False)   
